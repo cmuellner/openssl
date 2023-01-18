fd6f4d8d9887e1954ea69890a1e673a33fdfffc7
Christoph Müllner <christoph.muellner@vrull.eu>
2023-01-18 11:57:33 +0100

    riscv: GCM: Simplify GCM calculation
    The existing GCM calculation provides some potential
    for further optimizations. Let's use the demo code
    from the RISC-V cryptography extension groups
    (https://github.com/riscv/riscv-crypto), which represents
    the extension architect's intended use of the clmul instruction.
    
    The GCM calculation depends on bit and byte reversal.
    Therefore, we use the corresponding instructions to do that
    (if available at run-time).
    
    The resulting computation becomes quite compact and passes
    all tests.
    
    Note, that a side-effect of this change is a reduced register
    usage in .gmult(), which opens the door for an efficient .ghash()
    implementation.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

--- include/crypto/riscv_arch.h
+++ include/crypto/riscv_arch.h
@@ -57,7 +57,6 @@ static const size_t kRISCVNumCaps =
 ;
 
 /* Extension combination tests. */
-#define RISCV_HAS_ZBB_AND_ZBC() (RISCV_HAS_ZBB() && RISCV_HAS_ZBC())
 #define RISCV_HAS_ZBKB_AND_ZKND_AND_ZKNE() (RISCV_HAS_ZKNB() && RISCV_HAS_ZKND() && RISCV_HAS_ZKNE())
 #define RISCV_HAS_ZKND_AND_ZKNE() (RISCV_HAS_ZKND() && RISCV_HAS_ZKNE())
 
